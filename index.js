779f878eee76da125e1bb69ee49e790ee10f1c07
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-11-29 19:34:35 +0200

    refactoring index.js

--- index.js
+++ index.js
@@ -1,18 +1,7 @@
-const express=require('express')
-const cors=require('cors')
-const morgan=require('morgan')
+
 const config=require('./utils/config')
-const  app=express()
-const router=require('./controller/route')
-app.use(cors())
-app.use(express.json())
-app.use(express.static('dist'))
+const app=require('./app')
 
-morgan.token('content',function(req,res){ return JSON.stringify(req.body)})
-
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
-
-app.use(router)
 
 
 const PORT=config.PORT || 3001
