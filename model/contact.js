0eeb3955942107649f035a930380fccacb558d07
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-09 18:14:50 +0200

    Creating model for DB data storage

---
+++ model/contact.js
@@ -0,0 +1,34 @@
+const config=require('../utils/config')
+const mongoose=require('mongoose')
+
+
+const contactSchema=new mongoose.Schema({
+  name:{
+    type:String,
+    minLength:[3,'name length is short'],
+    required:true
+  },
+  number:{
+    type:String,
+    minLength:8,
+    required:[true,'user number required'],
+    validate:{
+      validator: function(v){
+        return /\d{2,3}-\d{7,8}/.test(v)
+      },
+      message:(props)=>`${props.value} is not valid number`
+    }
+
+  }
+})
+
+contactSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+
+module.exports=mongoose.model('Contact',contactSchema)
