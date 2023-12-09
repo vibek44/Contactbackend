6928795a766186d1fd3c97391d9b070a159785f9
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-09 18:35:09 +0200

    db connection functionality  painted

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 
 const config=require('./utils/config')
+const mongoose=require('mongoose')
 const app=require('./app')
 
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+  .then((result)=>{
+    console.log('connection succesful');
+    
+  })
+  .catch(err=>{
+    console.log('unsuccessful connection',err);
+    
+  })
 
 
 const PORT=config.PORT || 3001
 
 app.listen(PORT, ()=>console.log(`server is running on ${PORT}`))
-
