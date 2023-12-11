f51228271ba77d5f155ad3c9ec12588d08fc1a3e
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-11 20:48:52 +0200

    adding mongoose connection part

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
+const config=require('./utils/config')
 const express=require('express')
+const app=express()
 const cors=require('cors')
 const router=require('./controller/route')
-const app=express()
 const logger=require('./utils/logger')
-const {unknownEndpoint,errorHandler}=require('./utils/middleware')
+const { unknownEndpoint,errorHandler }=require('./utils/middleware')
+const mongoose=require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connection succesful')  
+  })
+  .catch(err => {
+    console.log('unsuccessful connection',err) 
+  })
 
 app.use(cors())
 app.use(express.static('dist'))
