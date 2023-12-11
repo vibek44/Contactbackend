bea0536087868ddd23dae4ed4424ee45796d4ec9
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-11 20:05:07 +0200

    correcting lint error

--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,6 @@
 const morgan=require('morgan')   //request logger
 
-morgan.token('content',function(req,res){ return JSON.stringify(req.body)})
+morgan.token('content',function(req){ return JSON.stringify(req.body)})
 
 const logger=morgan(':method :url :status :res[content-length] - :response-time ms :content')
 
