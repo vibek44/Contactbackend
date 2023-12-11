2b4a63dbde63b082964b189bce691836dca69ebb
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-11 19:57:21 +0200

    lint configuration  file added

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  'env': {
+    'node': true,
+    'commonjs': true,
+    'es2021': true
+  },
+  'extends': 'eslint:recommended',
+  'overrides': [
+    {
+      'env': {
+        'node': true
+      },
+      'files': [
+        '.eslintrc.{js,cjs}'
+      ],
+      'parserOptions': {
+        'sourceType': 'script'
+      }
+    }
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest'
+  },
+  'rules': {
+    'indent':[
+      'error',
+      2
+    ],
+    'quotes':[
+      'error',
+      'single'
+    ],
+    'semi':[
+      'error',
+      'never'
+    ],
+    'eqeqeq':'error',
+    'object-curly-spacing':['error','always'],
+    'arrow-spacing':[ 'error', { 'before':true,'after':true }]
+
+  }
+}
