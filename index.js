a5420b14f49ea1f50c2149935492fc254f420fd6
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2023-12-11 20:48:11 +0200

    moving mongoose connection part  to app.js

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const config=require ('./utils/config')
-const mongoose=require('mongoose')
 const app=require('./app')
-
-mongoose.set('strictQuery', false)
-
-mongoose.connect(config.MONGODB_URI)
-  .then(() =>{
-    console.log('connection succesful')  
-  })
-  .catch(err=>{
-    console.log('unsuccessful connection',err) 
-  })
 
 const PORT=config.PORT || 3001
 
-app.listen(PORT, ()=>console.log(`server is running on ${PORT}`))
+app.listen(PORT, () => console.log(`server is running on ${PORT}`))
